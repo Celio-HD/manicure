d56ccf8da909d81e1845fd534c76dfbc9bc6f89d
Celio Junior <jcelio060@gmail.com>
2026-06-04 13:49:21 -0300

    feat: add Tailwind CSS configuration and integrate with Vite

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f8f0ff',
+          100: '#f3e2ff',
+          200: '#e5c6ff',
+          300: '#d59bff',
+          400: '#c874ff',
+          500: '#b645f1',
+          600: '#8e33c1',
+          700: '#6a2690',
+          800: '#4e1f71',
+          900: '#3a1653',
+        },
+      },
+    },
+  },
+}
